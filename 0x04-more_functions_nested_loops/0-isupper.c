2deb9490e9c5428e8a66fd4aded5f86360b4ef5f
starktonn <smacharia498@gmail.com>
2023-03-23 13:08:17 -0700

    final try

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,10 @@
 #include "main.h"
 
-
-int main(void){
-	int _isupper(int c){
+int _isupper(int c){
         if(c >= 'A' && c <='Z'){
             return 1;
         }else{
             return 0;
         }
-    }
-	return (0)
 }
 
