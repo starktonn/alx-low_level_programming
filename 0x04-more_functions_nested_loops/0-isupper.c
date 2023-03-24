310242a35c28bd4b245d5eb06c094558b5b914b9
starktonn <smacharia498@gmail.com>
2023-03-23 20:34:23 -0700

    Write a function that checks for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
-int _isupper(int c){
-        if(c >= 'A' && c <='Z'){
+/**
+ * _isupper return 1 or 0 depending on provided input
+ * c in the character input
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if(c >= 'A' && c <='Z'){
             return 1;
         }else{
             return 0;
